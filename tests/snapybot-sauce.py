65e0115e52975a287dc8fa9f56dbb278dbd2fbe1
Vincent Gariepy <caktux@gmail.com>
2013-11-06 15:14:09 -0500

    Add Sauce test and update regular Selenium test

---
+++ tests/snapybot-sauce.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import unittest, time, re
+
+class SnapySauce(unittest.TestCase):
+    def setUp(self):
+        caps = webdriver.DesiredCapabilities.CHROME
+        caps['platform'] = "Linux"
+        caps['version'] = ""
+
+        caps['name'] = 'snapybot.com on Sauce'
+
+        self.driver = webdriver.Remote(
+            desired_capabilities=caps,
+            command_executor="http://caktux:9f6d2e20-fa00-418e-a418-790d8731058e@ondemand.saucelabs.com:80/wd/hub"
+        )
+        self.driver.implicitly_wait(10)
+
+        # Add Sauce sessionId for nose-sauce
+        self.sessionId = self.driver.session_id
+
+    def test_snapy_sauce(self):
+        driver = self.driver
+        driver.get("http://snapybot.com/")
+        driver.find_element_by_link_text("Easy").click()
+        driver.find_element_by_link_text("WHAT IS IT").click()
+        driver.find_element_by_link_text("DOCS").click()
+        driver.find_element_by_link_text("FAQ").click()
+        driver.find_element_by_link_text("CONTACT").click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/snapybot.py
+++ tests/snapybot.py
@@ -22,4 +22,4 @@ class Snapybot(unittest.TestCase):
         driver = self.driver
         driver.get(self.base_url + "/")
         driver.save_screenshot('screenshot.png')
-        driver.find_element_by_link_text("ABOUT").click()
+        driver.find_element_by_link_text("WHAT IS IT").click()
